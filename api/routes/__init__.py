7776cac650beacad479de67680a209352c850f6e
levonmendall <levon_mendall@yahoo.com>
2026-07-25 07:28:23 -0700

    Register personal CIO API routes

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -4,6 +4,7 @@ from api.routes.daily import router as daily_router
 from api.routes.decisions import router as decisions_router
 from api.routes.environment import router as environment_router
 from api.routes.health import router as health_router
+from api.routes.personal import router as personal_router
 from api.routes.portfolios import router as portfolios_router
 from api.routes.replays import router as replays_router
 
@@ -12,6 +13,7 @@ __all__ = [
     "decisions_router",
     "environment_router",
     "health_router",
+    "personal_router",
     "portfolios_router",
     "replays_router",
 ]
