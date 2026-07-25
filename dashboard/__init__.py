4d07e4b0a3b6da6e339b2679faad246af35048f7
levonmendall <levon_mendall@yahoo.com>
2026-07-24 21:49:35 -0700

    Add daily intelligence dashboard package

---
+++ dashboard/__init__.py
@@ -0,0 +1,5 @@
+"""Presentation helpers for the Capital Intelligence dashboard."""
+
+from dashboard.daily_intelligence import DailyIntelligenceView, build_daily_intelligence_view
+
+__all__ = ["DailyIntelligenceView", "build_daily_intelligence_view"]
