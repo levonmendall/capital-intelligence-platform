57181d845d6477031782317acd6ec3718baf9603
levonmendall <levon_mendall@yahoo.com>
2026-07-25 07:47:31 -0700

    Register authentication and user routers

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -1,5 +1,6 @@
 """Versioned API routers."""
 
+from api.routes.authentication import router as authentication_router
 from api.routes.daily import router as daily_router
 from api.routes.decisions import router as decisions_router
 from api.routes.environment import router as environment_router
@@ -7,8 +8,10 @@ from api.routes.health import router as health_router
 from api.routes.personal import router as personal_router
 from api.routes.portfolios import router as portfolios_router
 from api.routes.replays import router as replays_router
+from api.routes.users import router as users_router
 
 __all__ = [
+    "authentication_router",
     "daily_router",
     "decisions_router",
     "environment_router",
@@ -16,4 +19,5 @@ __all__ = [
     "personal_router",
     "portfolios_router",
     "replays_router",
+    "users_router",
 ]
