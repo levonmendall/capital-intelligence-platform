04dff8603ef4092f3f662941dc02f8db820a544e
levonmendall <levon_mendall@yahoo.com>
2026-07-26 08:45:25 -0700

    Export canonical CIO journal

--- journal/__init__.py
+++ journal/__init__.py
@@ -1,5 +1,17 @@
 """Institutional decision journals."""
 
+from cio.persistence import (
+    CIOJournalEvent,
+    CIOJournalEventType,
+    CIOJournalIntegrityError,
+    SQLiteCIOJournal,
+    serialize_candidate_decision,
+    serialize_cio_decision,
+    serialize_opportunity_queue,
+    serialize_specialist_packet,
+    serialize_thesis_review,
+    serialize_thesis_snapshot,
+)
 from journal.append_only import (
     JournalEvent,
     JournalEventType,
@@ -14,14 +26,24 @@ from journal.append_only import (
 from journal.decision_journal import DecisionJournalEntry
 
 __all__ = [
+    "CIOJournalEvent",
+    "CIOJournalEventType",
+    "CIOJournalIntegrityError",
     "DecisionJournalEntry",
     "JournalEvent",
     "JournalEventType",
     "JournalIntegrityError",
     "SQLiteAppendOnlyJournal",
+    "SQLiteCIOJournal",
+    "serialize_candidate_decision",
+    "serialize_cio_decision",
     "serialize_decision_quality_review",
     "serialize_market_change_assessment",
+    "serialize_opportunity_queue",
     "serialize_portfolio_fit_decision",
     "serialize_regime_committee_decision",
     "serialize_regime_run",
+    "serialize_specialist_packet",
+    "serialize_thesis_review",
+    "serialize_thesis_snapshot",
 ]
