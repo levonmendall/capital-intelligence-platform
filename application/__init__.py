682d7f19e1e82548e66df7e3c08a4cab833beea4
levonmendall <levon_mendall@yahoo.com>
2026-07-27 08:06:10 -0700

    feat: export production context assembly

--- application/__init__.py
+++ application/__init__.py
@@ -4,6 +4,18 @@ from application.production_cio import (
     ProductionCanonicalCIOContext,
     ProductionCanonicalCIOContextProvider,
     ProductionCanonicalCIOExecutor,
+    ProductionContextManifest,
+)
+from application.production_context import (
+    EvidenceCertificationState,
+    GovernedEvidenceLineage,
+    ProductionCandidateEvidence,
+    ProductionContextError,
+    ProductionContextEvidenceSnapshot,
+    ProductionHoldingEvidence,
+    RepositoryProductionCanonicalCIOContextProvider,
+    SQLiteProductionContextStore,
+    build_production_context_provider,
 )
 from application.daily_intelligence import (
     DailyCapitalIntelligenceService,
@@ -17,15 +29,25 @@ from application.daily_intelligence import (
 )
 
 __all__ = [
-    "ProductionCanonicalCIOContext",
-    "ProductionCanonicalCIOContextProvider",
-    "ProductionCanonicalCIOExecutor",
     "DailyCapitalIntelligenceService",
     "DailyCapitalIntelligenceSnapshot",
     "DailyIntelligenceCycle",
     "DailyIntelligenceStatus",
     "DailySnapshotRecord",
+    "EvidenceCertificationState",
+    "GovernedEvidenceLineage",
+    "ProductionCandidateEvidence",
+    "ProductionCanonicalCIOContext",
+    "ProductionCanonicalCIOContextProvider",
+    "ProductionCanonicalCIOExecutor",
+    "ProductionContextError",
+    "ProductionContextEvidenceSnapshot",
+    "ProductionContextManifest",
+    "ProductionHoldingEvidence",
+    "RepositoryProductionCanonicalCIOContextProvider",
     "SQLiteDailySnapshotStore",
+    "SQLiteProductionContextStore",
     "build_daily_capital_intelligence_snapshot",
+    "build_production_context_provider",
     "daily_snapshot_to_dict",
 ]
