4a0c260c4ae526d37956df8ca4744ab8b8aa2d5b
levonmendall <levon_mendall@yahoo.com>
2026-07-27 20:34:42 -0700

    Export free provider adapters and verifier

--- providers/__init__.py
+++ providers/__init__.py
@@ -1,5 +1,15 @@
 """External data providers for the Capital Intelligence Platform."""
 
+from providers.crypto_venues import (
+    CoinbaseExchangeProvider,
+    CryptoVenueBinding,
+    CryptoVenueBindingRegistry,
+    CryptoVenueProviderError,
+    KrakenSpotProvider,
+    build_coinbase_exchange_provider,
+    build_kraken_spot_provider,
+    load_crypto_venue_bindings,
+)
 from providers.fred import FREDRetrievalPolicy
 from providers.fred_cache import (
     FREDCache,
@@ -7,6 +17,30 @@ from providers.fred_cache import (
     JsonFREDCache,
     MemoryFREDCache,
     fred_cache_key,
+)
+from providers.free_connections import (
+    FreeProviderConnectionCatalog,
+    FreeProviderConnectionError,
+    FreeProviderConnectionIntegrityError,
+    FreeProviderConnectionReport,
+    FreeProviderConnectionState,
+    FreeProviderConnectionVerifier,
+    FreeProviderDefinition,
+    FreeProviderProbeResult,
+    SQLiteFreeProviderConnectionStore,
+    load_free_provider_catalog,
+)
+from providers.gleif import (
+    GleifEntityRecord,
+    GleifProvider,
+    GleifProviderError,
+)
+from providers.openfigi import (
+    OpenFigiInstrumentMatch,
+    OpenFigiMappingJob,
+    OpenFigiMappingResult,
+    OpenFigiProvider,
+    OpenFigiProviderError,
 )
 from providers.public_live_information import (
     PublicLiveCoverageReport,
@@ -29,13 +63,34 @@ from providers.sec_edgar import (
 )
 
 __all__ = [
+    "CoinbaseExchangeProvider",
+    "CryptoVenueBinding",
+    "CryptoVenueBindingRegistry",
+    "CryptoVenueProviderError",
     "FREDCache",
     "FREDCacheRecord",
     "FREDRetrievalPolicy",
+    "FreeProviderConnectionCatalog",
+    "FreeProviderConnectionError",
+    "FreeProviderConnectionIntegrityError",
+    "FreeProviderConnectionReport",
+    "FreeProviderConnectionState",
+    "FreeProviderConnectionVerifier",
+    "FreeProviderDefinition",
+    "FreeProviderProbeResult",
+    "GleifEntityRecord",
+    "GleifProvider",
+    "GleifProviderError",
     "GovernedPublicLiveInformationProvider",
     "ImpactfulPublicLiveInformationProvider",
     "JsonFREDCache",
+    "KrakenSpotProvider",
     "MemoryFREDCache",
+    "OpenFigiInstrumentMatch",
+    "OpenFigiMappingJob",
+    "OpenFigiMappingResult",
+    "OpenFigiProvider",
+    "OpenFigiProviderError",
     "PublicLiveCoverageReport",
     "PublicLiveInformationError",
     "PublicLiveInformationProvider",
@@ -44,6 +99,11 @@ __all__ = [
     "PublicLiveSourceResult",
     "SECEdgarProvider",
     "SECEdgarProviderError",
+    "SQLiteFreeProviderConnectionStore",
+    "build_coinbase_exchange_provider",
+    "build_kraken_spot_provider",
     "fred_cache_key",
+    "load_crypto_venue_bindings",
+    "load_free_provider_catalog",
     "load_public_live_source_catalog",
 ]
